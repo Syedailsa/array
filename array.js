881ecedb3f8d4bc978f1d900a674aa34a4b749dc
Ilsa. <ilsasyeda@gmail.com>
2024-03-07 19:50:04 +0500

    first commit

---
+++ array.js
@@ -0,0 +1,6 @@
+var names = [
+    "Ayesha",
+    "Zamal",
+    "Miraal",
+];
+console.log(names[0]);
---
+++ array.ts
@@ -0,0 +1,7 @@
+const names = [
+    "Ayesha",
+    "Zamal",
+    "Miraal",
+];
+
+console.log(names[0]);
