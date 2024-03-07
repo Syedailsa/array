f7176db09b90884ee1cb4026a650bea44e34772d
Ilsa. <ilsasyeda@gmail.com>
2024-03-07 21:28:27 +0500

    replace an item fron array

--- array.js
+++ array.js
@@ -1,8 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var guest = [
     "ubaida",
     "urooj",
     "ali",
 ];
+var index = guest.indexOf("urooj");
+guest[index] = "umair";
 guest.forEach(function (guest) {
     console.log("we would like to invite ".concat(guest, ", at the birthday party"));
 });
--- array.ts
+++ array.ts
@@ -1,8 +1,12 @@
-const guest = [
+let guest = [
     "ubaida",
     "urooj",
     "ali",
 ];
+let index = guest.indexOf("urooj");
+guest[index] = "umair";
+
 guest.forEach (guest => {
     console.log(`we would like to invite ${guest}, at the birthday party`)
 });
+export{}
